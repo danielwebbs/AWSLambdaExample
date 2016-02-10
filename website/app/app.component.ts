45e28fa94187b53cc8a5b0513049567272459e24
Divan Visagie <visagiedivan@gmail.com>
2016-02-10 08:50:51 +0200

    fix build location

--- website/app/app.component.ts
+++ website/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from 'angular2/core';
 
 @Component({
   selector: 'doge-app',
-  template: '<h1>My Doge App<h1/>'
+  template: '<h1>My Doge App</h1>'
 })
 
 export class AppComponent {}
